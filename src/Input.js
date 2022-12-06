1b738a35a1891b5a6c234a8a85d57dbcc9a0c38a
sungjihyun <jhsung23@gmail.com>
2022-12-07 01:18:00 +0900

    feat: 예상 숫자 입력의 유효성 검사 기능

---
+++ src/Input.js
@@ -0,0 +1,32 @@
+class Input {
+  constructor(input) {
+    this.validate(input);
+    this.input = input;
+  }
+
+  validate(input) {
+    throw new Error('This method must be implemented');
+  }
+}
+
+class NumberInput extends Input {
+  constructor(input) {
+    super(input.trim());
+    this.input = +input.trim();
+  }
+
+  validate(input) {
+    if (input.length !== 3) {
+      throw new Error('길이 제한 초과');
+    }
+
+    if (!/([1-9])/g.test(input)) {
+      throw new Error('유효한 숫자값이 아님');
+    }
+
+    if ([...input].some((target) => input.includes(target))) {
+      throw new Error('중복된 값이 있음');
+    }
+  }
+}
+module.exports = Input;
