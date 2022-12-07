d9dd0f100c21623873b6196748ed86dbd0d5893a
sungjihyun <jhsung23@gmail.com>
2022-12-08 00:29:10 +0900

    fix: 예상 숫자와 게임 커맨드 입력의 유효성 검사가 올바르게 되지 않는 이슈 해결

--- src/model/Input.js
+++ src/model/Input.js
@@ -12,19 +12,19 @@ class Input {
 class NumberInput extends Input {
   constructor(input) {
     super(input.trim());
-    this.input = +input.trim();
+    this.input = input.trim();
   }
 
   validate(input) {
     if (input.length !== 3) {
-      throw new Error('길이 제한 초과');
+      throw new Error('길이 제한 오류');
     }
 
-    if (!/([1-9])/g.test(input)) {
+    if (input.match(/([1-9])/g)?.length !== 3) {
       throw new Error('유효한 숫자값이 아님');
     }
 
-    if ([...input].some((target) => input.includes(target))) {
+    if (new Set([...input]).size !== 3) {
       throw new Error('중복된 값이 있음');
     }
   }
@@ -36,9 +36,10 @@ class CommandInput extends Input {
   }
 
   validate(input) {
-    if (input !== '1' || input !== '2') {
+    if (input !== '1' && input !== '2') {
       throw new Error('올바른 커맨드가 아님');
     }
   }
 }
-module.exports = Input;
+
+module.exports = { Input, NumberInput, CommandInput };
