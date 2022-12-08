bcc5815cc5b0c1fa443589506acadbea39533ec3
sungjihyun <jhsung23@gmail.com>
2022-12-08 14:22:11 +0900

    chore: 주석 삭제

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,7 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = require('./OutputView');
-const { NumberInput, CommandInput } = require('../Input');
+const { NumberInput, CommandInput } = require('../model/Input');
 
 const InputView = {
   read(message, handler) {
@@ -15,12 +15,10 @@ const InputView = {
     });
   },
 
-  // TODO handler 만들기
   readNumber(handler) {
     this.read('숫자를 입력해주세요 : ', (input) => handler(new NumberInput(input)));
   },
 
-  // TODO handler 만들기
   readCommand(handler) {
     this.read('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) =>
       handler(new CommandInput(input)),
