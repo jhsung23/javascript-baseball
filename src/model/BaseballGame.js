13e9b1b48901511127f48cf5e8453c08584b5aa0
sungjihyun <jhsung23@gmail.com>
2022-12-08 14:17:45 +0900

    feat: 볼과 스트라이크의 개수를 세는 기능

--- src/model/BaseballGame.js
+++ src/model/BaseballGame.js
@@ -6,6 +6,30 @@ class BaseballGame {
     this.#player = player;
     this.#computer = computer;
   }
+
+  play() {
+    const playerNumber = this.#player.getPlayerNumber();
+    const computerNumber = this.#computer.getComputerNumber();
+
+    const ball = this.countBall(playerNumber, computerNumber);
+    const strike = this.countStrike(playerNumber, computerNumber);
+
+    return { ball, strike };
+  }
+
+  countBall(playerNumber, computerNumber) {
+    return playerNumber.reduce((ballCount, targetNumber, index) => {
+      return computerNumber.includes(targetNumber) && computerNumber[index] !== targetNumber
+        ? ballCount + 1
+        : ballCount;
+    }, 0);
+  }
+
+  countStrike(playerNumber, computerNumber) {
+    return playerNumber.reduce((strikeCount, targetNumber, index) => {
+      return computerNumber[index] === targetNumber ? strikeCount + 1 : strikeCount;
+    }, 0);
+  }
 }
 
 module.exports = BaseballGame;
