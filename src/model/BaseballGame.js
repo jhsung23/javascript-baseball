cda48aee041f1dd7ebb97c77968261ff9f6a3b35
sungjihyun <jhsung23@gmail.com>
2022-12-08 14:17:00 +0900

    feat: 숫자야구 게임에 플레이어와 컴퓨터 주입

---
+++ src/model/BaseballGame.js
@@ -0,0 +1,11 @@
+class BaseballGame {
+  #player;
+  #computer;
+
+  constructor(player, computer) {
+    this.#player = player;
+    this.#computer = computer;
+  }
+}
+
+module.exports = BaseballGame;
